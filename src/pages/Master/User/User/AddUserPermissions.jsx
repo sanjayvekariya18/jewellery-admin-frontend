22ce3adcc9fce7d6b15f699e0c58b6a68d242d31
karan <karan@karmadhi.com>
2023-10-18 16:31:11 +0530

    addpermission bug fix

--- src/pages/Master/User/User/AddUserPermissions.jsx
+++ src/pages/Master/User/User/AddUserPermissions.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { API, HELPER } from "../../../../services";
 import {
-	Box,
-	Button,
-	Checkbox,
-	Icon,
-	IconButton,
-	Tooltip,
+  Box,
+  Button,
+  Checkbox,
+  Icon,
+  IconButton,
+  Tooltip,
 } from "@mui/material";
 import { SimpleCard } from "../../../../components";
 import { apiConfig } from "../../../../config";
@@ -15,192 +15,239 @@ import SimpleTable from "../../../../components/UI/SimpleTable";
 import ThemeDialog from "../../../../components/UI/Dialog/ThemeDialog";
 
 const AddUserPermissions = ({ open, togglePopup, userId, refreshTable }) => {
-	const [userPermissions, setUserPermissions] = useState({});
+  const [userPermissions, setUserPermissions] = useState({});
 
-	const getTableData = () => {
-		API.get(`${apiConfig.userPermission}/${userId}/not`).then((response) => {
-			const _userPermissions = {};
-			for (const group of Object.keys(response)) {
-				_userPermissions[group] = response[group].map((permissions) => {
-					return {
-						permissionMasterId: permissions.id,
-						permissionName: permissions.permissionName,
-						view: true,
-						create: false,
-						edit: false,
-						delete: false,
-					};
-				});
-			}
-			setUserPermissions(_userPermissions);
-		});
-	};
+  const getTableData = () => {
+    API.get(`${apiConfig.userPermission}/${userId}/not`).then((response) => {
+      const _userPermissions = {};
+      for (const group of Object.keys(response)) {
+        _userPermissions[group] = response[group].map((permissions) => {
+          return {
+            permissionMasterId: permissions.id,
+            permissionName: permissions.permissionName,
+            view: true,
+            create: false,
+            edit: false,
+            delete: false,
+          };
+        });
+      }
+      setUserPermissions(_userPermissions);
+    });
+  };
 
-	useEffect(() => {
-		if (userId && open === true) {
-			getTableData();
-		}
-		if (open === false) {
-			setUserPermissions({});
-		}
-	}, [open]);
+  useEffect(() => {
+    if (userId && open === true) {
+      getTableData();
+    }
+    if (open === false) {
+      setUserPermissions({});
+    }
+  }, [open]);
 
-	const toggleGroupPermissions = (group, value) => {
-		const _userPermissions = { ...userPermissions };
-		for (const permissions of _userPermissions[group]) {
-			permissions.view = !permissions.view;
-			permissions.create = !permissions.create;
-			permissions.edit = !permissions.edit;
-			permissions.delete = !permissions.delete;
-		}
-		setUserPermissions(_userPermissions);
-	};
+  const toggleGroupPermissions = (group, value) => {
+    const _userPermissions = { ...userPermissions };
+    let isAnyUnselected = _userPermissions[group].some((_permission) => {
+      return !_permission.create || !_permission.edit || !_permission.delete;
+    });
 
-	const toggleAllPermissions = (group, permissionMasterId, value) => {
-		const _userPermissions = { ...userPermissions };
-		const permissionIndex = _userPermissions[group].findIndex((row) => row.permissionMasterId === permissionMasterId);
-		if (permissionIndex >= 0) {
-			_userPermissions[group][permissionIndex].view = value;
-			_userPermissions[group][permissionIndex].create = value;
-			_userPermissions[group][permissionIndex].edit = value;
-			_userPermissions[group][permissionIndex].delete = value;
-			setUserPermissions(_userPermissions);
-		}
-	};
+    let hasUserPermission = isAnyUnselected == true;
 
-	const togglePermission = (group, permissionMasterId, operation, value) => {
-		const _userPermissions = { ...userPermissions };
-		const permissionIndex = _userPermissions[group].findIndex((row) => row.permissionMasterId === permissionMasterId);
-		if (permissionIndex >= 0) {
-			_userPermissions[group][permissionIndex][operation] = value;
-			setUserPermissions(_userPermissions);
-		}
-	};
+    for (const permissions of _userPermissions[group]) {
+      permissions.view = true;
+      permissions.create = hasUserPermission;
+      permissions.edit = hasUserPermission;
+      permissions.delete = hasUserPermission;
+    }
+    setUserPermissions(_userPermissions);
+  };
 
-	const addUserPermissions = () => {
-		let __userPermissions = Object.values({ ...userPermissions }).flat(1)
+  const toggleAllPermissions = (group, permissionMasterId, value) => {
+    const _userPermissions = { ...userPermissions };
+    const permissionIndex = _userPermissions[group].findIndex(
+      (row) => row.permissionMasterId === permissionMasterId
+    );
+    if (permissionIndex >= 0) {
+      _userPermissions[group][permissionIndex].view = true;
+      _userPermissions[group][permissionIndex].create = value;
+      _userPermissions[group][permissionIndex].edit = value;
+      _userPermissions[group][permissionIndex].delete = value;
+      setUserPermissions(_userPermissions);
+    }
+  };
 
-		let payload = {
-			userId: userId,
-			permissionDetails: __userPermissions
-		}
+  const togglePermission = (group, permissionMasterId, operation, value) => {
+    const _userPermissions = { ...userPermissions };
+    const permissionIndex = _userPermissions[group].findIndex(
+      (row) => row.permissionMasterId === permissionMasterId
+    );
+    if (permissionIndex >= 0) {
+      _userPermissions[group][permissionIndex][operation] = value;
+      setUserPermissions(_userPermissions);
+    }
+  };
 
-		API.post(apiConfig.userPermission, payload)
-			.then(() => {
-				HELPER.toaster.success('Permission added!')
-				refreshTable();
-			})
+  const addUserPermissions = () => {
+    let __userPermissions = Object.values({ ...userPermissions }).flat(1);
 
-	}
+    let payload = {
+      userId: userId,
+      permissionDetails: __userPermissions,
+    };
 
-	const userPermissionHeaderColumns = [
-		{
-			headerCell: ({ group }) => (
-				<Tooltip title="Add/Remove All">
-					<IconButton onClick={(e) => toggleGroupPermissions(group, e.target.checked)}>
-						<Icon color="error">add</Icon>
-					</IconButton>
-				</Tooltip>
-			),
-			align: "left",
-			width: "30%",
-			cell: ({ item, group }) => (
-				<Checkbox
-					color="secondary"
-					checked={item.create === true && item.edit === true && item.delete === true}
-					onChange={(e) => toggleAllPermissions(group, item.permissionMasterId, e.target.checked)}
-				/>
-			)
-		},
-		{
-			headerCell: "Permission",
-			align: "left",
-			width: "30%",
-			cell: ({ item }) => {
-				return item.permissionName
-			}
-		},
-		{
-			headerCell: "View",
-			align: "left",
-			width: "30%",
-			cell: ({ item }) => (
-				<ThemeSwitch disabled name="view" checked={item.view} />
-			)
-		},
-		{
-			headerCell: "Create",
-			align: "left",
-			width: "30%",
-			cell: ({ item, group }) => (
-				<ThemeSwitch
-					name="create"
-					checked={item.create}
-					color="success"
-					onChange={(e) => togglePermission(group, item.permissionMasterId, e.target.name, e.target.checked)}
-				/>
-			)
-		},
-		{
-			headerCell: "Edit",
-			align: "left",
-			width: "30%",
-			cell: ({ item, group }) => (
-				<ThemeSwitch
-					name="edit"
-					checked={item.edit}
-					color="warning"
-					onChange={(e) => togglePermission(group, item.permissionMasterId, e.target.name, e.target.checked)}
-				/>
-			)
-		},
-		{
-			headerCell: "Delete",
-			align: "left",
-			width: "30%",
-			cell: ({ item, group }) => (
-				<ThemeSwitch
-					name="delete"
-					checked={item.delete}
-					color="error"
-					onChange={(e) => togglePermission(group, item.permissionMasterId, e.target.name, e.target.checked)}
-				/>
-			)
-		},
-	];
+    API.post(apiConfig.userPermission, payload).then(() => {
+      HELPER.toaster.success("Permission added!");
+      refreshTable();
+    });
+  };
 
-	return (
-		<>
-			<ThemeDialog
-				title={`Add Permissions`}
-				isOpen={open}
-				onClose={togglePopup}
-				maxWidth="lg"
-				actionBtns={
-					<>
-						<Box>
-							<Button variant="outlined" color="secondary" onClick={togglePopup}>
-								Cancel
-							</Button>
-							<Button type="submit" color="primary" onClick={() => addUserPermissions()}>
-								Save
-							</Button>
-						</Box>
-					</>
-				}
-			>
-				{Object.keys(userPermissions).map((group, i) => (
-					<SimpleCard key={i} title={group} sx={{ mb: 2 }}>
-						<SimpleTable
-							data={userPermissions[group]}
-							headerColumns={userPermissionHeaderColumns}
-							extraData={{ group }}
-						/>
-					</SimpleCard>
-				))}
-			</ThemeDialog>
-		</>
-	);
+  const userPermissionHeaderColumns = [
+    {
+      headerCell: ({ group }) => (
+        <Tooltip title="Add/Remove All">
+          <IconButton
+            onClick={(e) => toggleGroupPermissions(group, e.target.checked)}
+          >
+            <Icon color="error">add</Icon>
+          </IconButton>
+        </Tooltip>
+      ),
+      align: "left",
+      width: "30%",
+      cell: ({ item, group }) => (
+        <Checkbox
+          color="secondary"
+          checked={
+            item.create === true && item.edit === true && item.delete === true
+          }
+          onChange={(e) =>
+            toggleAllPermissions(
+              group,
+              item.permissionMasterId,
+              e.target.checked
+            )
+          }
+        />
+      ),
+    },
+    {
+      headerCell: "Permission",
+      align: "left",
+      width: "30%",
+      cell: ({ item }) => {
+        return item.permissionName;
+      },
+    },
+    {
+      headerCell: "View",
+      align: "left",
+      width: "30%",
+      cell: ({ item }) => (
+        <ThemeSwitch disabled name="view" checked={item.view} />
+      ),
+    },
+    {
+      headerCell: "Create",
+      align: "left",
+      width: "30%",
+      cell: ({ item, group }) => (
+        <ThemeSwitch
+          name="create"
+          checked={item.create}
+          color="success"
+          onChange={(e) =>
+            togglePermission(
+              group,
+              item.permissionMasterId,
+              e.target.name,
+              e.target.checked
+            )
+          }
+        />
+      ),
+    },
+    {
+      headerCell: "Edit",
+      align: "left",
+      width: "30%",
+      cell: ({ item, group }) => (
+        <ThemeSwitch
+          name="edit"
+          checked={item.edit}
+          color="warning"
+          onChange={(e) =>
+            togglePermission(
+              group,
+              item.permissionMasterId,
+              e.target.name,
+              e.target.checked
+            )
+          }
+        />
+      ),
+    },
+    {
+      headerCell: "Delete",
+      align: "left",
+      width: "30%",
+      cell: ({ item, group }) => (
+        <ThemeSwitch
+          name="delete"
+          checked={item.delete}
+          color="error"
+          onChange={(e) =>
+            togglePermission(
+              group,
+              item.permissionMasterId,
+              e.target.name,
+              e.target.checked
+            )
+          }
+        />
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <ThemeDialog
+        title={`Add Permissions`}
+        isOpen={open}
+        onClose={togglePopup}
+        maxWidth="lg"
+        actionBtns={
+          <>
+            <Box>
+              <Button
+                variant="outlined"
+                color="secondary"
+                onClick={togglePopup}
+              >
+                Cancel
+              </Button>
+              <Button
+                type="submit"
+                color="primary"
+                onClick={() => addUserPermissions()}
+              >
+                Save
+              </Button>
+            </Box>
+          </>
+        }
+      >
+        {Object.keys(userPermissions).map((group, i) => (
+          <SimpleCard key={i} title={group} sx={{ mb: 2 }}>
+            <SimpleTable
+              data={userPermissions[group]}
+              headerColumns={userPermissionHeaderColumns}
+              extraData={{ group }}
+            />
+          </SimpleCard>
+        ))}
+      </ThemeDialog>
+    </>
+  );
 };
 
 export default AddUserPermissions;
